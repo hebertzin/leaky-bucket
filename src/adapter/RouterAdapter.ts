b3c8c147f39d75f79038e2a15aff9920d735b8c2
hebertsanto <hebertsantosdeveloper@gmail.com>
2025-04-28 21:42:54 -0300

    feat: create an adapter router

---
+++ src/adapter/RouterAdapter.ts
@@ -0,0 +1,29 @@
+import { Context } from "koa";
+import { Controller, HttpResponse } from "../domain/controller";
+
+export const adaptRoute = (controller: Controller) => {
+    return async (ctx: Context) => {
+        try {
+            const response: HttpResponse = await controller.handle(ctx.request);
+
+            if (response.statusCode >= 200 && response.statusCode <= 299) {
+                ctx.status = response.statusCode;
+                ctx.body = {
+                    response,
+                };
+            } else {
+                ctx.status = response.statusCode;
+                ctx.body = {
+                    message: response.msg,
+                    statusCode: response.statusCode,
+                };
+            }
+        } catch (error: any) {
+            ctx.status = error?.statusCode || 500;
+            ctx.body = {
+                msg: "An unexpected error occurred.",
+                error: error?.message || String(error),
+            };
+        }
+    };
+};
