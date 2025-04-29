7db5df050549fa83807961fbdaea71c1d27d2991
hebertsanto <hebertsantosdeveloper@gmail.com>
2025-04-29 12:26:46 -0300

    feat: create factory and user route

--- server.ts
+++ server.ts
@@ -1,15 +1,18 @@
 import Koa from "koa";
 import { startDatabase } from "./src/infra/database/Mongo";
+import userRoutes from "./src/presentation/routes/UsersRoutes";
+
 
 const app = new Koa();
 
+startDatabase()
+
+app.use(userRoutes.routes())
 app.use(async (ctx) => {
   ctx.body = "Hello from Koa + TypeScript!";
 });
 
 const PORT = process.env.PORT || 3000;
-
-startDatabase()
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
