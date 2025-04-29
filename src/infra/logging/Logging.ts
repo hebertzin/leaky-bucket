2d26105213b98c20deae898eaedd7486a15a5c11
hebertsanto <hebertsantosdeveloper@gmail.com>
2025-04-29 12:11:34 -0300

    feat: implementations of logging and bcrypt in infra layer

---
+++ src/infra/logging/Logging.ts
@@ -0,0 +1,50 @@
+import { createLogger, transports, format, Logger } from "winston";
+import { Logging } from "../../domain/Logging";
+
+class LoggerService implements Logging {
+    private logger: Logger;
+
+    constructor() {
+        this.logger = createLogger({
+            level: "info",
+            format: format.combine(
+                format.timestamp(),
+                format.json(),
+                format.colorize(),
+                format.printf(({ level, message, timestamp }) => {
+                    return `${timestamp} ${level}: ${message}`;
+                }),
+            ),
+            transports: [
+                new transports.Console(),
+                new transports.File({
+                    filename: "error.log",
+                    level: "error",
+                }),
+                new transports.File({ filename: "combined.log" }),
+            ],
+        });
+    }
+
+    info(message: string) {
+        this.logger.info(message);
+    }
+
+    error(message: string) {
+        this.logger.error(message);
+    }
+
+    warn(message: string) {
+        this.logger.warn(message);
+    }
+
+    debug(message: string) {
+        this.logger.debug(message);
+    }
+
+    log(level: string, message: string) {
+        this.logger.log({ level, message });
+    }
+}
+
+export const logging = new LoggerService();
