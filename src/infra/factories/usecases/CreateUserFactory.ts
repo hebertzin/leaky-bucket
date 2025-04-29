a4187ede5f35b1fc2d1e1600f429cdb231aa1260
hebertsanto <hebertsantosdeveloper@gmail.com>
2025-04-29 12:11:44 -0300

    feat: create user factory

---
+++ src/infra/factories/usecases/CreateUserFactory.ts
@@ -0,0 +1,13 @@
+
+import { CreateUserUseCase } from "../../../application/usecases/user/CreateUserUseCase"
+import { mongo } from "../../database/Mongo"
+import { MongoUsersRepository } from "../../database/repository/UsersRepository"
+import { logging } from "../../logging/Logging"
+import { BcryptHashService } from "../../security/Hash"
+
+
+export const makeDbSaveUser = () => {
+    const repository = new MongoUsersRepository(mongo.getDatabase())
+    const bcrypt = new BcryptHashService()
+    return new CreateUserUseCase(repository, bcrypt, logging)
+}
